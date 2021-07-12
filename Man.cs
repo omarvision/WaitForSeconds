132a7c131b9c751825b60d340d5953e8b2183719
Omar Santiago <omarkenosia@gmail.com>
2021-07-11 21:31:14 -0400

    Add files via upload

---
+++ Man.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public GameObject OliveOil = null;
+    public GameObject Character = null;
+
+    public void Start()
+    {
+        OliveOil.SetActive(false);
+        Character.SetActive(false);
+
+        //ShowCharacters();
+
+        StartCoroutine(WaitBeforeShow());
+    }
+
+    private void ShowCharacters()
+    {
+        OliveOil.SetActive(true);
+
+        //wait a couple of seconds
+
+        Character.SetActive(true);
+    }
+
+    private IEnumerator WaitBeforeShow()
+    {
+        OliveOil.SetActive(true);
+
+        yield return new WaitForSeconds(5);
+
+        Character.SetActive(true);
+    }
+
+}
